1e24ee5741acfcbeddd0416543487244b21d1f27
lievertom <lievertom@gmail.com>
2020-10-11 10:31:17 -0300

    rename file

---
+++ projeto-1/programa/inc/actuator.h
@@ -0,0 +1,40 @@
+#ifndef ACTUATOR_H_
+#define ACTUATOR_H_
+
+/* Header includes */
+#include <bcm2835.h>
+
+#define FAN RPI_V2_GPIO_P1_18 
+#define RESISTANCE RPI_V2_GPIO_P1_16
+#define ON 0
+#define OFF 1
+
+/*!
+ * @brief Function to configure the pins
+ *
+ * @return void
+ *
+ */
+// void configuration();
+
+/*!
+ *  @brief Function to handle preogram interuption.
+ *
+ *  @param[in] signal        :  interruption signal
+ * 
+ *  @return void.
+ *
+ */
+void shut_down(int signal);
+
+/*!
+ * @brief Function to initialize the sensor
+ *
+ * @return void
+ *
+ */
+void initialize_actuators();
+
+void on_off_actuators(int fan, int resistance);
+
+#endif /* ACTUATOR_H_ */
---
+++ projeto-1/programa/src/actuator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "actuator.h"
+
+void configuration()
+{
+    bcm2835_gpio_fsel(FAN, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(RESISTANCE, BCM2835_GPIO_FSEL_OUTP);
+}
+
+void shut_down(int signal)
+{
+    on_off_actuators(OFF, OFF);
+    bcm2835_close();
+    exit(0);
+}
+
+void initialize_actuators()
+{
+    if (!bcm2835_init())
+      exit(1);
+      
+    configuration();
+
+}
+
+void on_off_actuators(int fan, int resistance)
+{
+    bcm2835_gpio_write(FAN, fan);
+    bcm2835_gpio_write(RESISTANCE, resistance);
+}
